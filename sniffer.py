1b1e4d82c0d1bbad61797d7d87a758791baab79f
Petit Jump <killianbonnin7@gmail.com>
2026-07-24 15:12:20 +0200

    Ajout

--- sniffer.py
+++ sniffer.py
@@ -11,7 +11,7 @@ with serial.Serial('COM3', 9600, timeout=5) as ser: # Boucle pour récuperer les
         data: bytes = ser.readline() # On récupere la ligne
         ligne: str = data.decode('utf-8').strip() # On la décode en utf-8 pour ne pas l'avoir en binaire
         valeurs: list = ligne.split(',') # On split 'data' pour crée une liste avec que les données (sans les ',')
-        with open('data.csv', 'a', newline='') as f:
+        with open('mesures.csv', 'a', newline='') as f:
             if ligne: # Vérifie que 'ligne' n'est pas vide
                 writer = csv.writer(f)
                 writer.writerow([time.strftime('%X %x')] + valeurs)
